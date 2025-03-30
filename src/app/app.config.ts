fe3bb11a56c18112ddd69bd0eac99bdc5def7a59
Sundaram14 <r.sundaram2015@gmail.com>
2025-03-30 08:47:08 +0530

    removed SSR

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,12 +3,11 @@ import { provideRouter } from '@angular/router';
 import { provideAnimations } from '@angular/platform-browser/animations';
 
 import { routes } from './app.routes';
-import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
-     provideAnimations(),
-     provideRouter(routes), 
-     provideClientHydration(withEventReplay())]
+    provideAnimations(),
+    provideRouter(routes)
+  ]
 };
