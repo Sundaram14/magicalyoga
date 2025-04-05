b3bcda450a36fa0ac8b1fd62f59ed5b5fa0d8516
Sundaram14 <r.sundaram2015@gmail.com>
2025-04-05 15:49:21 +0530

    Enhanced SEO

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,10 +7,20 @@ import { IntroductionComponent } from './components/introduction/introduction.co
 
 
 export const routes: Routes = [
+  {
+    path: 'sitemap.xml',
+    redirectTo: '/assets/sitemap.xml',
+    pathMatch: 'full',
+  },
+  {
+    path: 'robots.txt',
+    redirectTo: '/assets/robots.txt',
+    pathMatch: 'full',
+  },  
   { path: '', component: HomeComponent },
   { path: 'summer-camp', component: SummerCampComponent },
   { path: 'video-contents', component: VideoContentsComponent },
   { path: 'contact-us', component: ContactUsComponent },
   { path: 'about-us', component: IntroductionComponent},
-   { path: '**', redirectTo: '' } // Redirect invalid routes to home
+  { path: '**', redirectTo: '' } // Redirect invalid routes to home
 ];
